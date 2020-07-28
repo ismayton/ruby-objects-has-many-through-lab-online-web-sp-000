fdd58aed776c62891c73eaa0713f82979e3d6bba
Ian Mayton <ian.mayton@gmail.com>
2020-07-28 17:45:06 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -13,6 +13,10 @@ class Patient
     @@all 
   end 
   
+  def new_appointment(date, doctor)
+    Appointment.new(date, self, doctor)
+  end 
+  
   def appointments
     Appointment.all.select {|appt| appt.patient == self} 
   end 
