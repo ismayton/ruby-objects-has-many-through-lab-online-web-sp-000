f0537e40ab8468b7c9e5d432be09d483b3fc18c1
Ian Mayton <ian.mayton@gmail.com>
2020-07-28 17:16:32 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,19 @@
 class Artist 
   
+  @@all = []
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def new_song(name, genre)
+    song = Song.new(name, genre)
+    song.artist = self 
+  end 
+  
+  def songs 
+    Song.all.select {|song| song.artist == self} 
+  end 
+  
+  
 end 
